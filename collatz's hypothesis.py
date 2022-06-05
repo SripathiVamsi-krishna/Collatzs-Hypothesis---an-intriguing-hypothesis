1d0c8de04569d66c1de4778142c0f878d11015a3
SripathiVamsi-krishna <103228606+SripathiVamsi-krishna@users.noreply.github.com>
2022-06-05 14:51:21 +0530

    Add files via upload
    In 1937, a German mathematician named Lothar Collatz formulated an intriguing hypothesis (it still remains unproven) which can be described in the following way:
    
    take any non-negative and non-zero integer number and name it c0;
    if it's even, evaluate a new c0 as c0 ÷ 2;
    otherwise, if it's odd, evaluate a new c0 as 3 × c0 + 1;
    if c0 ≠ 1, skip to point 2.
    The hypothesis says that regardless of the initial value of c0, it will always go to 1.
    
    Of course, it's an extremely complex task to use a computer in order to prove the hypothesis for any natural number (it may even require artificial intelligence), but you can use Python to check some individual numbers. Maybe you'll even find the one which would disprove the hypothesis.
    
    
    Write a program which reads one natural number and executes the above steps as long as c0 remains different from 1. We also want you to count the steps needed to achieve the goal. Your code should output all the intermediate values of c0, too.
    Sample input: 23
    
    Expected output:
    
    70
    35.0
    106.0
    53.0
    160.0
    80.0
    40.0
    20.0
    10.0
    5.0
    16.0
    8.0
    4.0
    2.0
    1.0
    The total steps count is  15

---
+++ collatz's hypothesis.py
@@ -0,0 +1,21 @@
+﻿c0 = int(input("Enter a non-zero and non-negative number: "))
+count = 0
+while (c0 > 1):
+    if (c0 % 2 == 0):
+        c0 = c0 / 2
+        print(c0)
+        count = count + 1
+        #print("The total no.of steps:", count)
+    elif (c0 % 2 != 0):
+        c0 = ((3 * c0) + 1)
+        print(c0)
+        count = count + 1
+        #print("The total no.of steps:", count)
+    elif (c0 == 1):
+        count = 1
+        #print("The total no.of steps:", count)
+        break
+    else:
+        print("Please enter a valid positive and non-zero number...:(((")
+print("The total steps count is ", count)
+        
